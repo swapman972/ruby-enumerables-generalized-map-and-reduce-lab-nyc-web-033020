902b734a0620e2945875ccd532ce36a2f57a4efd
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 00:40:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,9 +4,10 @@ def map (source_array, block)
   new_array = []
   counter = 0
   while counter < source_array.length do
-    new_array << yield(source_array[counter])
+    new_array << source_array[counter]
     counter += 1
   end
+  new_array.map do block end
   new_array
 end
 
